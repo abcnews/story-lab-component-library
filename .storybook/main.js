1c60699d3217a8324f3d84fb5ccbfcea0b6cbea6
Simon Elvery <simon@elvery.net>
2020-07-06 14:54:26 +1000

    Overhaul project structure

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,33 +1,76 @@
-const path = require("path");
-
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const isDevelopment = process.env.NODE_ENV === 'development';
 module.exports = {
-  stories: ["../src/**/*.stories.tsx"],
-  // Add any Storybook addons you want here: https://storybook.js.org/addons/
-  addons: [],
-  webpackFinal: async (config) => {
-    config.module.rules.push({
-      test: /\.scss$/,
-      use: [
-        "style-loader",
-        {
-          loader: "css-loader",
-          options: {
-            modules: true,
-          },
-        },
-        "sass-loader",
-      ],
-      include: path.resolve(__dirname, "../"),
-    });
+  stories: ['../stories/**/*.stories.(ts|tsx)'],
+  addons: [
+    '@storybook/addon-actions',
+    '@storybook/addon-knobs/register',
+    '@storybook/addon-links',
+    '@storybook/addon-docs',
+  ],
+  webpackFinal: async config => {
+    config.plugins.push(
+      new MiniCssExtractPlugin({
+        filename: isDevelopment ? '[name].css' : '[name].[hash].css',
+        chunkFilename: isDevelopment ? '[id].css' : '[id].[hash].css',
+      })
+    );
 
-    config.module.rules.push({
-      test: /\.(ts|tsx)$/,
-      loader: require.resolve("babel-loader"),
-      options: {
-        presets: [["react-app", { flow: false, typescript: true }]],
+    config.module.rules.push(
+      {
+        test: /\.module\.s(a|c)ss$/,
+        loader: [
+          isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              sourceMap: isDevelopment,
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: isDevelopment,
+            },
+          },
+        ],
       },
-    });
-    config.resolve.extensions.push(".ts", ".tsx");
+      {
+        test: /\.s(a|c)ss$/,
+        exclude: /\.module.(s(a|c)ss)$/,
+        loader: [
+          isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: isDevelopment,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        use: [
+          {
+            loader: require.resolve('ts-loader'),
+            options: {
+              transpileOnly: true,
+            },
+          },
+          {
+            loader: require.resolve('react-docgen-typescript-loader'),
+            options: {
+              tsconfigPath: path.resolve(__dirname, '../tsconfig.json'),
+            },
+          },
+        ],
+      }
+    );
+
+    config.resolve.extensions.push('.ts', '.tsx', '.scss');
 
     return config;
   },
--- src/EmbedContainer/EmbedContainer.tsx
+++ src/EmbedContainer/EmbedContainer.tsx
@@ -1,10 +1,30 @@
-import React from "react";
-import styles from "./EmbedContainer.scss";
+import React, { PropsWithChildren } from 'react';
+import styles from './EmbedContainer.module.scss';
 
-export const EmbedContainer = ({ children, embed, className }) => {
+/**
+ * EmbedContainer props
+ */
+export interface IEmbedContainerProps {
+  /** Defines the embed style and position */
+  embed?: 'full' | 'right';
+  /** An additional className string added to the property for custom styling */
+  className?: string;
+}
+
+export type EmbedContainerProps = PropsWithChildren<IEmbedContainerProps>;
+export type EmbedContainerComponent = React.FC<EmbedContainerProps>;
+
+/**
+ * Use `EmbedContainer` to mimic the style of embedded components in Presentation Layer.
+ */
+export const EmbedContainer: EmbedContainerComponent = ({
+  children,
+  embed,
+  className,
+}: EmbedContainerProps) => {
   const cns = [styles.embedContainer];
-  if (embed === "right") cns.push(styles.right);
-  if (embed === "full") cns.push(styles.full);
-  cns.push(className);
-  return <aside className={cns.join(" ")}>{children}</aside>;
+  if (embed === 'right') cns.push(styles.right);
+  if (embed === 'full') cns.push(styles.full);
+  if (className) cns.push(className);
+  return <aside className={cns.join(' ')}>{children}</aside>;
 };
